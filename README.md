187cdb0417b36d7908f9e7df98c4011874f1de3e
Sergey Dyachenko <sdyachenko1977@gmail.com>
2023-11-12 13:31:49 +0300

    added example

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # zeebe
 Здесь буду выкладывать материалы которые использую при изучении.
 
+StateChanger - проект простого воркера на dotnet. Его задача пока что просто логгировать вызовы.
+
+StateChanger.bpmn - схема, в которой есть задача, вызывающая сервис.
+
+docker-compose.yaml - скрипт запускает кластер из 3 инстансов Zeebe.
+
+gateway.proto - прото grpc, для построения клиента/админки.
+
+Управление Zeebe через команду https://github.com/camunda-community-hub/zbctl-via-npm
+
+Для запуска примера нужно:
+
+1. "docker-compose up". Убедиться что кластер стартовал.
+2. "zbctl deploy StateChanger.bpmn --insecure". Команда должна вернуть json, из нее нужно взять bpmnProcessId (например Process_1oi88v7).
+3. Запустить сервис StateChanger, убедиться что он подключился к кластеру - не должно быть ошибок в консоли.
+3. "zbctl create instance Process_1oi88v7 --insecure".
+4. В логе сервиса должно быть видно, что произошел его вызов.
---
+++ StateChanger/Program.cs
@@ -0,0 +1,62 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Zeebe.Client;
+using Zeebe.Client.Api.Responses;
+using Zeebe.Client.Api.Worker;
+using Zeebe.Client.Impl.Builder;
+
+Console.WriteLine("Zeebe worker started");
+var zeebeClient = ZeebeClient.Builder()
+    .UseGatewayAddress("localhost:26500")
+    .UsePlainText()
+    .Build();
+
+var topology = await zeebeClient.TopologyRequest().Send();
+var test = topology.Brokers;
+
+using (var signal = new EventWaitHandle(false, EventResetMode.AutoReset))
+{
+    zeebeClient.NewWorker()
+        .JobType("ChangeState")
+        .Handler(HandleJob)
+        .MaxJobsActive(5)
+        .Name("ChangeState")
+        .AutoCompletion()
+        .PollInterval(TimeSpan.FromSeconds(1))
+        .Timeout(TimeSpan.FromSeconds(10))
+        .Open();
+ 
+    // blocks main thread, so that worker can run
+    signal.WaitOne();
+}
+
+Console.ReadLine();
+
+static void HandleJob(IJobClient jobClient, IJob job)
+{
+    // business logic
+    var jobKey = job.Key;
+    Console.WriteLine("Handling job: " + job);
+ 
+    if (jobKey % 3 == 0)
+    {
+        jobClient.NewCompleteJobCommand(jobKey)
+            .Variables("{\"foo\":2}")
+            .Send()
+            .GetAwaiter()
+            .GetResult();
+    }
+    else if (jobKey % 2 == 0)
+    {
+        jobClient.NewFailCommand(jobKey)
+            .Retries(job.Retries - 1)
+            .ErrorMessage("Example fail")
+            .Send()
+            .GetAwaiter()
+            .GetResult();
+    }
+    else
+    {
+        // auto completion
+    }
+}
